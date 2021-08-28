c9804994d69bae42d742caebb8df22fd79487a1f
Kanishk Sharma <kanishksh10@gmail.com>
2021-08-29 01:37:54 +0530

    added app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  templateUrl: '../app/app.component.html',
+  styleUrls: ['../app/app.component.sass']
 })
 export class AppComponent {
   title = 'conFusion';
